03e89f91cf570ce00c7758cd5bb52ecb50e4f6e4
Fatima Azzahro <azzahro.fatima@gmail.com>
2020-07-31 09:02:30 +0700

    Script - Show clicked number in Calculator screen

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
-    console.log(event.target.value);
+    updateScreen(event.target.value);
   });
 });
+
+const calculatorScreen = document.querySelector(".calculator-screen");
+
+const updateScreen = (number) => {
+  calculatorScreen.value = number;
+};
