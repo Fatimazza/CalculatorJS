d47870d2865cd800f4b53f4cec24b6eef67b7024
Fatima Azzahro <azzahro.fatima@gmail.com>
2020-07-31 09:36:41 +0700

    Script - Save inputted number to current number

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+let prevNumber = "";
+let currentNumber = "";
+let calculatorOperator = "";
+
+const inputNumber = (number) => {
+  currentNumber = number;
+};
+
 const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
-    updateScreen(event.target.value);
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
   });
 });
 
